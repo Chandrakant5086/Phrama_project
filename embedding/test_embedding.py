c3a0ff4398506bc1951dccef06bcd2e5f22fa3f5
Chandrakant5086 <chandrakantbharti1997@gmail.com>
2026-01-13 06:52:39 +0000

    shape  changes in np  array

--- embedding/test_embedding.py
+++ embedding/test_embedding.py
@@ -1,14 +1,14 @@
 from embedding.embedding_generator import generate_embedding
 from utils.logger import log
-
+import numpy as np
 
 chunks = [
     "This molecule showed high stability in LCMS analysis.",
     "Unexpected impurity observed during synthesis."
 ]
 
-embeddings = generate_embedding(chunks)
+embeddings = np.asarray(generate_embedding(chunks))
 
-if embeddings is not None:
-    print("Embeddings shape:", embeddings.shape)
-log("test embedding succesfully")
+print("Embeddings shape:", embeddings.shape)
+
+log("test embedding successfully")
